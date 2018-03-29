ef21d4299757b5c0e319b6d077ff1474bc085dd1
Sam Wrigley <sam@samwrigley.co.uk>
2018-03-29 17:26:02 +0100

    Created the `CanBePublished` trait in order to easily set an item to published, scheduled or draft

---
+++ src/Traits/CanBePublished.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace SamWrigley\Support\Traits;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+
+trait CanBePublished
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $canBePublishedFillable = [
+        'published_at',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $canBePublishedDates = [
+        'published_at'
+    ];
+
+    /**
+     * Boot the current trait.
+     *
+     * @return void
+     */
+    public static function bootCanBePublished()
+    {
+        static::retrieved(
+            function ($model) {
+                $model->fillable = array_merge(
+                    $model->fillable,
+                    $model->canBePublishedFillable
+                );
+
+                $model->dates = array_merge(
+                    $model->dates,
+                    $model->canBePublishedDates
+                );
+            }
+        );
+    }
+
+    /**
+     * Set item to published at given datetime.
+     *
+     * @param \Illuminate\Support\Carbon $publishedDateTime
+     * @return void
+     */
+    public function publish(Carbon $publishedDateTime)
+    {
+        $this->published_at = $publishedDateTime;
+
+        $this->save();
+    }
+
+    /**
+     * Set item to draft.
+     *
+     * @return void
+     */
+    public function draft()
+    {
+        $this->published_at = null;
+
+        $this->save();
+    }
+
+    /**
+     * Check if item is published.
+     *
+     * @return boolean
+     */
+    public function isPublished()
+    {
+        return $this->published_at >= Carbon::now();
+    }
+
+    /**
+     * Check if item is scheduled.
+     *
+     * @return boolean
+     */
+    public function isScheduled()
+    {
+        return $this->published_at < Carbon::now();
+    }
+
+    /**
+     * Check if item is draft.
+     *
+     * @return boolean
+     */
+    public function isDraft()
+    {
+        return is_null($this->published_at);
+    }
+
+    /**
+     * Scope a query to published items.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopePublished(Builder $query)
+    {
+        return $query
+            ->whereNotNull('published_at')
+            ->where('published_at', '>=', Carbon::now());
+    }
+
+    /**
+     * Scope a query to scheduled items.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeScheduled(Builder $query)
+    {
+        return $query
+            ->whereNotNull('published_at')
+            ->where('published_at', '<', Carbon::now());
+    }
+
+    /**
+     * Scope a query to draft items.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeDraft(Builder $query)
+    {
+        return $query->whereNull('published_at');
+    }
+
+    /**
+     * Scope a query by published month.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param \Illuminate\Support\Carbon $month
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWhereMonth(Builder $query, Carbon $month)
+    {
+        return $query->whereMonth('published_at', $month);
+    }
+
+    /**
+     * Scope a query by published year.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param \Illuminate\Support\Carbon $year
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWhereYear(Builder $query, Carbon $year)
+    {
+        return $query->whereYear('published_at', $year);
+    }
+
+    /**
+     * Scope a query to items published between given datetimes.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param \Illuminate\Support\Carbon $startDateTime
+     * @param \Illuminate\Support\Carbon $endDateTime
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWhereBetween(Builder $query, Carbon $startDateTime, Carbon $endDateTime)
+    {
+        return $query->whereBetween('published_at', [$startDateTime, $endDateTime]);
+    }
+
+    /**
+     * Scope a query to items published before given datetime.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param \Illuminate\Support\Carbon $beforeDateTime
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWhereBefore(Builder $query, Carbon $beforeDateTime)
+    {
+        return $query->where('published_at', '<', $beforeDateTime);
+    }
+
+    /**
+     * Scope a query to items published after given datetime.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param \Illuminate\Support\Carbon $afterDateTime
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWhereAfter(Builder $query, Carbon $afterDateTime)
+    {
+        return $query->where('published_at', '>', $afterDateTime);
+    }
+}
